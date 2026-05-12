40535b15874ad3c30dba49a1d91423e8e1c977e1
gzh <elecdot5@gmail.com>
2026-05-12 16:28:16 +0800

    fix: add IOBUF clarify for open drain i2c

--- vivado/ip_repo/src/i2c_open_drain_io.v
+++ vivado/ip_repo/src/i2c_open_drain_io.v
@@ -8,9 +8,35 @@ module i2c_open_drain_io (
     output wire scl_in,
     output wire sda_in
 );
+`ifdef SYNTHESIS
+    IOBUF #(
+        .DRIVE(12),
+        .IBUF_LOW_PWR("TRUE"),
+        .IOSTANDARD("LVCMOS33"),
+        .SLEW("SLOW")
+    ) u_i2c_scl_iobuf (
+        .I(1'b0),
+        .O(scl_in),
+        .T(~scl_drive_low),
+        .IO(i2c_scl)
+    );
+
+    IOBUF #(
+        .DRIVE(12),
+        .IBUF_LOW_PWR("TRUE"),
+        .IOSTANDARD("LVCMOS33"),
+        .SLEW("SLOW")
+    ) u_i2c_sda_iobuf (
+        .I(1'b0),
+        .O(sda_in),
+        .T(~sda_drive_low),
+        .IO(i2c_sda)
+    );
+`else
     assign i2c_scl = scl_drive_low ? 1'b0 : 1'bz;
     assign i2c_sda = sda_drive_low ? 1'b0 : 1'bz;
 
     assign scl_in = i2c_scl;
     assign sda_in = i2c_sda;
+`endif
 endmodule
